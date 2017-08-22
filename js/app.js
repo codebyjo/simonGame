bad7d127d3f92a196fc091d98fa2fa18caab00b0
Joanne D <codebyjo@gmail.com>
2017-08-21 21:05:56 -0400

    add javascript game requirequirements

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
-User Story: I am presented with a random series of button presses.
 
-User Story: Each time I input a series of button presses correctly, I see the same series of button presses but with an additional step.
 
-User Story: I hear a sound that corresponds to each button both when the series of button presses plays, and when I personally press a button.
 
-User Story: If I press the wrong button, I am notified that I have done so, and that series of button presses starts again to remind me of the pattern so I can try again.
+//-- start game
 
-User Story: I can see how many steps are in the current series of button presses.
+//-- start round
+//I am presented with a random series of button presses.
 
-User Story: If I want to restart, I can hit a button to do so, and the game will return to a single step.
+//-- add random number (1 to 4) to sequence
+//Each time I input a series of button presses correctly, I see the same series of button presses but with an additional step.
 
-User Story: I can play in strict mode where if I get a button press wrong, it notifies me that I have done so, and the game restarts at a new random series of button presses.
+//-- animate sequence
 
-User Story: I can win the game by getting a series of 20 steps correct. I am notified of my victory, then the game starts over.
+//-- allow user to press buttons and register clicks
+//I can see how many steps are in the current series of button presses.
+//I hear a sound that corresponds to each button both when the series of button presses plays, and when I personally press a button.
+
+//-- if user is wrong, buzz
+//If I press the wrong button, I am notified that I have done so, and that series of button presses starts again to remind me of the pattern so I can try again.
+
+//-- if number of clicks is less than length of sequence, continue waiting for player clicks
+
+//-- restart button
+//If I want to restart, I can hit a button to do so, and the game will return to a single step.
+
+//win when:
+//I can win the game by getting a series of 20 steps correct. I am notified of my victory, then the game starts over.
+
+// strict mode (hard)
+//I can play in strict mode where if I get a button press wrong, it notifies me that I have done so, and the game restarts at a new random series of button presses.
